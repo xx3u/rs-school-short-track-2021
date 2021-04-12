564d3ae7c13f8c78c6ce0ce03e1aaeb6968eb24c
Yuliya Khvan <yuliya.khv@gmail.com>
2021-04-12 23:10:45 +0600

    feat: add solution to 12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const newArr = [];
+  const count = [];
+  for (let i = 0; i < domains.length; i++) {
+    newArr.push(domains[i]);
+    let sum = 0;
+    for (let j = 0; j < domains.length; j++) {
+      if (domains[j].includes(domains[i])) {
+        sum += 1;
+      }
+    }
+    count.push(sum);
+  }
+  const arr = [];
+  for (let i = 0; i < domains.length; i++) {
+    arr[i] = domains[i].split('.');
+    const x = [];
+    for (let j = arr[i].length - 1; j >= 0; j--) {
+      x.push(arr[i][j]);
+    }
+    arr[i] = x.join('.');
+  }
+  let obj = {};
+  if (domains.length !== 0) {
+    obj = { '.com': domains.length };
+    for (let k = 0; k < arr.length; k++) {
+      obj['.'.concat(arr[k])] = count[k];
+      obj = { ...obj, ...obj[arr[k]] };
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
